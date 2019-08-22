54efbde73a34b2894d40397abbcb1b330afc2a13
José Flores <josecruzflores.fl@gmail.com>
2019-08-22 14:54:10 -0500

    Add is_letter util

--- lexer/utils.py
+++ lexer/utils.py
@@ -1,3 +1,5 @@
+import re
+
 def is_digit(s: str) -> bool:
     try:
         int(s)
@@ -7,3 +9,8 @@ def is_digit(s: str) -> bool:
 
 def is_operator(s: str) -> bool:
     return s in "+-*/"
+
+regex = re.compile('[a-zA-Z]')
+
+def is_letter(s: str) -> bool:
+    return regex.fullmatch(s) is not None
