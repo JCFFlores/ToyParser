1e01da2abc2ed56899692afc68ffd3c35dfa77ad
José Flores <josecruzflores.fl@gmail.com>
2019-08-23 12:34:57 -0500

    Complete lexer

--- lexer/lexer.py
+++ lexer/lexer.py
@@ -1,6 +1,8 @@
 from enum import Enum
-from typing import Mapping, Union
-from .exceptions import NotComplexCharacterException
+from typing import Mapping, Union, Generator, Callable
+from dataclasses import dataclass
+from functools import partial
+from .exceptions import InvalidTokenException
 from .utils import is_digit, is_operator, is_letter
 
 
@@ -18,23 +20,28 @@ class ComplexCharacter(Enum):
     OPERATOR = 3
 
 
-State = Union[TokenType, int]
-Character = Union[str, ComplexCharacter]
-START = 0
-INTERMEDIATE = 1
+class NonTerminalState(Enum):
+    START = 1
+    VARIABLE_INTERMEDIATE = 2
 
-automaton: Mapping[State, Mapping[Character, State]] = {
-    START: {
+
+State = Union[TokenType, NonTerminalState]
+Character = Union[str, ComplexCharacter]
+Automata = Mapping[State, Mapping[Character, State]]
+
+automata: Automata = {
+    NonTerminalState.START: {
         ComplexCharacter.DIGIT: TokenType.NUMBER,
-        '$': INTERMEDIATE,
+        '$': NonTerminalState.VARIABLE_INTERMEDIATE,
         '(': TokenType.LEFT_PARENTHESIS,
         ')': TokenType.RIGHT_PARENTHESIS,
-        ComplexCharacter.OPERATOR: TokenType.OPERATOR
+        ComplexCharacter.OPERATOR: TokenType.OPERATOR,
+        ' ': NonTerminalState.START,
     },
     TokenType.NUMBER: {
         ComplexCharacter.DIGIT: TokenType.NUMBER
     },
-    INTERMEDIATE: {
+    NonTerminalState.VARIABLE_INTERMEDIATE: {
         ComplexCharacter.LETTER: TokenType.VARIABLE
     },
     TokenType.VARIABLE: {
@@ -43,11 +50,44 @@ automaton: Mapping[State, Mapping[Character, State]] = {
     }
 }
 
-def checkComplexCharacter(s: str) -> ComplexCharacter:
+
+def convert_character(s: str) -> Character:
     if is_digit(s):
         return ComplexCharacter.DIGIT
     if is_letter(s):
         return ComplexCharacter.LETTER
     if is_operator(s):
         return ComplexCharacter.OPERATOR
-    raise NotComplexCharacterException
+    return s
+
+
+@dataclass
+class Token:
+    token_type: TokenType
+    value: str
+
+
+def token_or_exception(state: State, token: str) -> Token:
+    if isinstance(state, TokenType):
+        return Token(state, token)
+    raise InvalidTokenException(token)
+
+
+def tokenize_helper(automata: Automata,
+                    text: str) -> Generator[Token, None, None]:
+    token: str = ""
+    current_state: State = NonTerminalState.START
+    for current_char, next_char in zip(text, text[1:] + ' '):
+        current_character: Character = convert_character(current_char)
+        next_character: Character = convert_character(next_char)
+        next_state: State = automata[current_state][current_character]
+        token += current_char
+        current_state = next_state
+        if automata[current_state].get(next_character):
+            yield token_or_exception(current_state, token.strip())
+            token = ""
+            current_state = NonTerminalState.START
+
+
+tokenize: Callable[[str], Generator[Token, None, None]
+                   ] = partial(tokenize_helper, automata)
