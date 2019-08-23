abda8887c76acaf638ddcc292dbdf2a67d2330a6
José Flores <josecruzflores.fl@gmail.com>
2019-08-23 13:29:47 -0500

    Add test_long_token and test_invalid_variable unit tests

--- test_lexer.py
+++ test_lexer.py
@@ -1,5 +1,6 @@
 from lexer import tokenize, Token, TokenType, InvalidTokenException, TokenGenerator
 from typing import List
+import pytest
 
 
 def assert_tokenize(token_list: List[Token], input_string: str) -> None:
@@ -23,5 +24,34 @@ def test_sum():
             '2'),
         Token(
             TokenType.RIGHT_PARENTHESIS,
-            ')')]
+            ')'),
+    ]
     assert_tokenize(token_list, input_string)
+
+
+def test_long_token():
+    input_string: str = "(- 1544 8693)"
+    token_list: List[Token] = [
+        Token(
+            TokenType.LEFT_PARENTHESIS,
+            '('),
+        Token(
+            TokenType.OPERATOR,
+            '-'),
+        Token(
+            TokenType.NUMBER,
+            '1544'),
+        Token(
+            TokenType.NUMBER,
+            '8693'),
+        Token(
+            TokenType.RIGHT_PARENTHESIS,
+            ')'),
+    ]
+    assert_tokenize(token_list, input_string)
+
+
+def test_invalid_variable():
+    input_string: str = "$1234"
+    with pytest.raises(InvalidTokenException):
+        list(tokenize(input_string))
