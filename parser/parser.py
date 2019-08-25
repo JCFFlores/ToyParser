cd7992c216201339172b261a3df7dcf82b707b6f
Jose Flores <josecruzflores.fl@gmail.com>
2019-08-25 17:31:19 -0500

    Complete parser

--- parser/parser.py
+++ parser/parser.py
@@ -1,7 +1,8 @@
 from typing import Deque, Callable
-from lexer import Token, TokenType
+from lexer import Token, TokenType, TokenGenerator
 from functools import partial
-from .exceptions import NoMoreTokensException, IncorrectTokenException
+from collections import deque
+from .exceptions import NoMoreTokensException, IncorrectTokenException, ExcedingTokensException
 
 TokenList = Deque[Token]
 ConsumerFunction = Callable[[TokenList], None]
@@ -48,3 +49,9 @@ def parse_start(token_list: TokenList) -> None:
     parse_expression(token_list)
     parse_expression(token_list)
     consume_right_parenthesis(token_list)
+
+
+def parse(token_list: TokenList) -> None:
+    parse_start(token_list)
+    if not len(token_list) == 0:
+        raise ExcedingTokensException
