95ac630e615df7672e5db23d93e32b3ca97b6976
Jose Flores <josecruzflores.fl@gmail.com>
2019-08-25 17:26:49 -0500

    Add ExcedingTokensException

--- parser/exceptions.py
+++ parser/exceptions.py
@@ -8,3 +8,7 @@ class IncorrectTokenException(Exception):
 
     def __init__(self, token: Token):
         super().__init__(f'Invalid expression on token: "{token.value}"')
+
+
+class ExcedingTokensException(Exception):
+    pass
