3960eeee4368bc852d99f8cfe12c47350f24c6ac
Jose Flores <josecruzflores.fl@gmail.com>
2019-08-25 18:44:58 -0500

    Add entry point in minicompiler for parser

--- minicompiler.py
+++ minicompiler.py
@@ -1,4 +1,6 @@
 from lexer import tokenize, InvalidTokenException
+from parser import TokenList, parse, IncorrectTokenException, ExcedingTokensException, NoMoreTokensException
+from collections import deque
 import click
 
 @click.group()
@@ -14,6 +16,23 @@ def scanner(input_string: str) -> None:
             print(token)
     except InvalidTokenException as e:
         print(e)
+
+
+@cli.command()
+@click.argument('input_string')
+def parser(input_string: str) -> None:
+    try:
+        token_list: TokenList = deque(tokenize(input_string))
+        parse(token_list)
+        print(f'{input_string} is a correct and valid expression')
+    except InvalidTokenException as e:
+        print(e)
+    except IncorrectTokenException as e:
+        print(e)
+    except ExcedingTokensException:
+        print(f'{input_string} is too long to be a valid expression')
+    except NoMoreTokensException:
+        print(f'{input_string} is to short to be a valid expression')
 
 
 if __name__ == '__main__':
