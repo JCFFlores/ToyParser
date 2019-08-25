a7c29735efc3c7aa8d49f4297d91457bde9cf192
Jose Flores <josecruzflores.fl@gmail.com>
2019-08-25 17:22:33 -0500

    Add parse functions for each state and reformat

--- parser/parser.py
+++ parser/parser.py
@@ -1,9 +1,11 @@
 from typing import Deque, Callable
 from lexer import Token, TokenType
+from functools import partial
 from .exceptions import NoMoreTokensException, IncorrectTokenException
 
 TokenList = Deque[Token]
 ConsumerFunction = Callable[[TokenList], None]
+
 
 def consume_token(token_type: TokenType, token_list: TokenList) -> None:
     try:
@@ -15,7 +17,34 @@ def consume_token(token_type: TokenType, token_list: TokenList) -> None:
 
 
 consume_number: ConsumerFunction = partial(consume_token, TokenType.NUMBER)
-consume_left_parenthesis: ConsumerFunction = partial(consume_token, TokenType.LEFT_PARENTHESIS)
-consume_right_parenthesis: ConsumerFunction = partial(consume_token, TokenType.RIGHT_PARENTHESIS)
+consume_left_parenthesis: ConsumerFunction = partial(
+    consume_token, TokenType.LEFT_PARENTHESIS)
+consume_right_parenthesis: ConsumerFunction = partial(
+    consume_token, TokenType.RIGHT_PARENTHESIS)
 consume_operator: ConsumerFunction = partial(consume_token, TokenType.OPERATOR)
 consume_variable: ConsumerFunction = partial(consume_token, TokenType.VARIABLE)
+
+
+def parse_expression(token_list: TokenList) -> None:
+    try:
+        next_token: Token = token_list[0]
+        if next_token.token_type is TokenType.LEFT_PARENTHESIS:
+            consume_left_parenthesis(token_list)
+            consume_operator(token_list)
+            parse_expression(token_list)
+            parse_expression(token_list)
+            consume_right_parenthesis(token_list)
+        elif next_token.token_type is TokenType.VARIABLE:
+            consume_variable(token_list)
+        else:
+            consume_number(token_list)
+    except IndexError:
+        raise NoMoreTokensException from None
+
+
+def parser_start(token_list: TokenList) -> None:
+    consume_left_parenthesis(token_list)
+    consume_operator(token_list)
+    parse_expression(token_list)
+    parse_expression(token_list)
+    consume_right_parenthesis(token_list)
